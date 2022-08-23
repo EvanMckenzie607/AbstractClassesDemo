56bffe9a138e85fd2ef258ab077ef2906ae12d6f
Evan Mckenzie <615evan@gmail.com>
2022-08-23 12:51:31 -0400

    Completed Exercise

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,34 +17,63 @@ namespace ConsoleUI
             #region Vehicles
 
             /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
+             * * Create an abstract class called Vehicle
+             * * The vehicle class shall have three string properties Year, Make, and Model
+             * * Set the defaults to something generic in the Vehicle class
+             * * Vehicle shall have an abstract method called DriveAbstract with no implementation
+             * * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
              */
 
             /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
+             * * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
+             * * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
+             * * Provide the implementations for the abstract methods
              * Only in the Motorcycle class will you override the virtual drive method
             */
 
             // Create a list of Vehicle called vehicles
-
+            var vehicles = new List<Vehicle>();
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * - new it up as one of each derived class
              * Set the properties with object initializer syntax
              */
+            Car fusion = new Car() { HasTrunk = true, Year = 2019, Make = "Ford", Model = "Fusion P-HEV"};
 
+            Motorcycle fatBoy = new Motorcycle()
+            {
+                HasSideCart = true,
+                Year = 2008,
+                Make = "Harley Davidson",
+                Model = "Fat Boy",
+            };
+            Vehicle prius = new Car() { HasTrunk = true, Year = 2005, Make = "Toyota", Model = "Prius" };
+            Vehicle ninja = new Motorcycle() { HasSideCart = false, Year = 2017, Make = "Kawasaki", Model = "Ninja" };
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+            vehicles.Add(fusion);
+            vehicles.Add(fatBoy);
+            vehicles.Add(prius);
+            vehicles.Add(ninja);
+
+            foreach(var vehicle in vehicles)
+            {
+                Console.WriteLine($"Make: {vehicle.Make}\n" +
+                    $"Model: {vehicle.Model}\n" +
+                    $"Year: {vehicle.Year}");
+                vehicle.DriveVirtual();
+                Console.WriteLine("----------------------");
+            }
 
             // Call each of the drive methods for one car and one motorcycle
+            fusion.DriveAbstract();
+            fusion.DriveVirtual();
+            fatBoy.DriveAbstract();
+            fatBoy.DriveVirtual();
+
+
 
             #endregion            
             Console.ReadLine();
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int Year { get; set; } = 2020;
+        public string Make { get; set; } = "Empty Make";
+        public string Model { get; set; } = "Empty Model";
+
+        public abstract void DriveAbstract();
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is in hyperDrive");
+        }
+
+    }
+}
